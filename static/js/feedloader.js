b46929292ef63ed85215da80c099a05b020c953a
Miguel Lopes <powerofthescript@outlook.com>
2021-12-14 22:25:57 +0000

    Update Website

--- static/js/feedloader.js
+++ static/js/feedloader.js
@@ -22,8 +22,8 @@ function RenderFeed() {
     var article = ParseFeed().getElementsByTagName("item")
     for (let index = 0; index < article.length; index++) {
         let image = article[index].getElementsByTagName("enclosure")[0].getAttribute("url")
-        image = "/static/img/pwrscript.png"
-        document.getElementById("blog").insertAdjacentHTML('afterbegin', '<div class="blog-post-card"> <div class="blog-data"><a href="'+article[index].getElementsByTagName("link")[0].textContent+'">  <h2>' + article[index].getElementsByTagName("title")[0].textContent + '</h2> </a><span>' + article[index].getElementsByTagName("description")[0].textContent + '</span></div></div>')
+        
+        document.getElementById("blog").insertAdjacentHTML('afterbegin', '<div class="blog-post-card blog-data"><p></p><img src="'+image+'"><a href="'+article[index].getElementsByTagName("link")[0].textContent+'">  <h2>' + article[index].getElementsByTagName("title")[0].textContent + '</h2> </a><span>' + article[index].getElementsByTagName("description")[0].textContent + '</span><p></p></div>')
         document.querySelector(".blog-post-card > a[href='https://www.pwrscript.dedyn.io']")
     }
     
--- static/js/nav.js
+++ static/js/nav.js
@@ -99,6 +99,7 @@ function Projects() {
     hideBlog()
     hideAboutMe();
     showProjects()
+    
 }
 
 function AboutMe() {
